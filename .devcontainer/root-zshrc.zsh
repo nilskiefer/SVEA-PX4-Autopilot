3d8e356216d83d95f3c44d0707211654329f591f
nils_kiefer <21311514+nilskiefer@users.noreply.github.com>
2026-05-09 17:08:54 +0000

    Fix: home dir agnostic zsh setup

--- .devcontainer/root-zshrc.zsh
+++ .devcontainer/root-zshrc.zsh
@@ -2,10 +2,10 @@
 # Keep installs in postCreateCommand; keep shell behavior here.
 
 # Enable plugin scripts installed by postCreateCommand.
-if [ -f /root/.oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh ]; then
-	source /root/.oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+if [ -f "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" ]; then
+	source "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh"
 fi
 
-if [ -f /root/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]; then
-	source /root/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+if [ -f "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ]; then
+	source "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 fi
